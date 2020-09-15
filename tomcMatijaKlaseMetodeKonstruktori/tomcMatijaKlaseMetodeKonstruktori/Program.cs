ca2111f29fe36bb49db76d681dffbaebc3423a52
matijatomc <55134628+matijatomc@users.noreply.github.com>
2020-09-15 11:18:22 +0200

    Add files via upload

---
+++ tomcMatijaKlaseMetodeKonstruktori/tomcMatijaKlaseMetodeKonstruktori/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tomcMatijaKlaseMetodeKonstruktori
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Proceduralni x = new Proceduralni();
+            Objektni y = new Objektni();
+            Funkcionalni z = new Funkcionalni();
+
+            Console.WriteLine("Upiši te postotak za Proceduralni tip jezika: ");
+            x.Postotak = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Upiši te postotak za Objektni tip jezika: ");
+            y.Postotak = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Upiši te postotak za Funkcionalni tip jezika: ");
+            z.Postotak = Convert.ToDouble(Console.ReadLine());
+
+            if (x.Postotak + y.Postotak + z.Postotak != 100)
+            {
+                Console.WriteLine("Neispravni podaci!!");
+            }
+            else
+            {
+                if (x.Postotak > y.Postotak && x.Postotak > z.Postotak)
+                {
+                    Console.WriteLine("Najzatupljeniji tip jezika je Proceduralni");
+                }
+                else if (y.Postotak > z.Postotak)
+                {
+                    Console.WriteLine("Najzatupljeniji tip jezika je Objektni");
+                }
+                else
+                {
+                    Console.WriteLine("Najzatupljeniji tip jezika je Funkcionalni");
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ tomcMatijaKlaseMetodeKonstruktori/tomcMatijaKlaseMetodeKonstruktori/ProgramskiJezik.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tomcMatijaKlaseMetodeKonstruktori
+{
+    class ProgramskiJezik
+    {
+        double postotak;
+
+        public double Postotak { get => postotak; set => postotak = value; }
+    }
+}
